ab852f4a3b41456e37be6491af52151b9b1f9e0d
conradosu <conrado_victor@yahoo.com.br>
2022-05-12 16:08:09 -0300

    index

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import { NextSeo } from 'next-seo';
+import styles from '../styles/index.module.css'
+
+const Home = () => {
+  return (
+    <>
+    <NextSeo title="Bag Rosa | LP"/>
+    </>
+  )
+}
+
+export default Home;
